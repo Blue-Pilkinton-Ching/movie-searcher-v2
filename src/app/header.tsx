1e1c3225496b62b8fe045fe794018b576b073109
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2024-02-11 20:04:18 +1300

    Started project

---
+++ src/app/header.tsx
@@ -0,0 +1,23 @@
+export function Header() {
+  //   function onSearch() {
+  //     con
+  //   }
+
+  return (
+    <header className="w-full h-fit min-h-24 flex items-center lg:justify-between flex-col *:h-full *:flex *:items-center lg:flex-row px-[5%]">
+      <div>
+        <div className="text-3xl p-5">Watchalot</div>
+      </div>
+      <div className="w-full px-[5%]">
+        <textarea
+          //   onKeyDown={onSearch}
+          className="flex-grow bg-neutral-900 h-10 resize-none focus:outline-none rounded line py-1.5 overflow-hidden px-5 text-lg"
+          name="search"
+          cols={1}
+          rows={1}
+          placeholder="Search for movies or TV shows..."
+        ></textarea>
+      </div>
+    </header>
+  )
+}
