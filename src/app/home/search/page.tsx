5a88e0035ceb380bc4b8ad4132bc448bada644f1
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2024-02-14 15:30:26 +1300

    Fix for deployment

--- src/app/home/search/page.tsx
+++ src/app/home/search/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import { Suspense } from 'react'
-import { MediaSearch } from '../../../../interfaces'
-import { Card } from '../card'
 import { useSearchParams } from 'next/navigation'
+import { Search } from './search'
 
 export default function Page() {
   const searchParams = useSearchParams()
@@ -22,35 +21,5 @@ export default function Page() {
         <Search search={search} />
       </Suspense>
     </main>
-  )
-}
-
-export async function Search({ search }: { search: string }) {
-  let media
-  try {
-    media = (await JSON.parse(
-      await (
-        await fetch(`/api/search?query=${encodeURIComponent(search)}`)
-      ).text()
-    )) as MediaSearch
-  } catch (error) {
-    return <p className="text-2xl font-bold">Something went wrong :(</p>
-  }
-
-  media.results = media.results.filter((x) => x.media_type != 'person')
-
-  if (media.results.length < 1) {
-    return <p className="text-2xl font-bold">Couldn&apos;t find anything :(</p>
-  }
-
-  return (
-    <>
-      <br />
-      <div className="flex flex-wrap gap-5 sm:gap-8 justify-evenly ">
-        {media.results.map((element, index) => {
-          return <Card key={index} data={element} />
-        })}
-      </div>
-    </>
   )
 }
